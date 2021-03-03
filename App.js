fcb49caa2af77dd742d1748535325e636548708b
Paul Lee <paul_lee@kakao.com>
2021-03-03 23:12:53 +0900

    setting AppLoading Async Function

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import React, {useState} from 'react';
 import {AppLoading} from "expo";
 import {Text, Image} from "react-native";
 import {Asset} from "expo-asset";
+import {Ionicons} from "@expo/vector-icons";
+import * as Font from 'expo-font';
 
 const cacheImages = images => images.map(image => {
 	if (typeof image === "string") {
 		return Image.prefetch(image)
-	}else{
-        return Asset.fromModule(image).downloadAsync();
-    }
-})
+	} else {
+		return Asset.fromModule(image).downloadAsync();
+	}
+});
+
+const cacheFonts = fonts => fonts.map(font => Font.loadAsync(font));
 
 export default function App() {
 	const [isReady, setIsReady] = useState(false)
@@ -19,7 +23,12 @@ export default function App() {
 			require("./assets/loginBg.jpg"),
 			"https://logopng.net/wp-content/uploads/2020/07/logo-airbnb-png-1.png"
 		];
-		console.log(cacheImages(images))
+		const fonts = [
+			Ionicons.font
+		]
+		const imagePromises = cacheImages(images);
+		const fontPromises = cacheFonts(fonts);
+		return Promise.all([...fontPromises, ...imagePromises])
 	}
 	return isReady ? (<Text>I'M ready</Text>) : (
 		<AppLoading onError={console.error} onFinish={handleFinish} startAsync={loadAssets}/>);
