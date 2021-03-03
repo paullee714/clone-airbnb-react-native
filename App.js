15a48def9abc8f8604dd38e8ca273454c536e300
Paul Lee <paul_lee@kakao.com>
2021-03-03 23:02:26 +0900

    App Load Setting

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {AppLoading} from "expo";
+import {Text, Image} from "react-native";
+import {Asset} from "expo-asset";
+
+const cacheImages = images => images.map(image => {
+	if (typeof image === "string") {
+		return Image.prefetch(image)
+	}else{
+        return Asset.fromModule(image).downloadAsync();
+    }
+})
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to Airbnb</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [isReady, setIsReady] = useState(false)
+	const handleFinish = () => setIsReady(true)
+	const loadAssets = async () => {
+		const images = [
+			require("./assets/loginBg.jpg"),
+			"https://logopng.net/wp-content/uploads/2020/07/logo-airbnb-png-1.png"
+		];
+		console.log(cacheImages(images))
+	}
+	return isReady ? (<Text>I'M ready</Text>) : (
+		<AppLoading onError={console.error} onFinish={handleFinish} startAsync={loadAssets}/>);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
